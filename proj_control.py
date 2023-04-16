58057bc4ec02414a6a5ec3cc4fe83d7cbd81a776
r-pettersson <61046614+r-pettersson@users.noreply.github.com>
2023-04-16 13:52:20 +0200

    Update proj_control.py

--- proj_control.py
+++ proj_control.py
@@ -1,6 +1,8 @@
 ##########################
 #Author: Roger Pettersson
 #Date: 2020-03-15
+#Date modified: 2023-04-16
+#Adapted for XW5000ES projector
 ##########################
 
 import paho.mqtt.client as mqtt #import the client1
@@ -26,11 +28,18 @@ validResponses = {
                 }
 
 #serial
-port = serial.Serial("/dev/ttyUSB1", baudrate=38400, timeout=0.1)
+port = serial.Serial("/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0", baudrate=38400, timeout=0.1)
+#port = serial.Serial("/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0", baudrate=9600, timeout=0.1)
 
-#array of commands for the strda5800es
+#array of commands for the xw5000es
 #reply is to be used later as a decoding machanism, reply=number of bytes received as a reply
-commands = { "proj_status":{"command":b'\xA9\x01\x02\x01\x00\x03\x03\x9A', 
+commands = { "proj_on":{"command":b'\xA9\x17\x2E\x00\x00\x00\x3F\x9A', 
+                         "reply":11, 
+                         },
+               "proj_off":{"command":b'\xA9\x17\x2F\x00\x00\x00\x3F\x9A', 
+                         "reply":11, 
+                         },                        
+              "proj_status":{"command":b'\xA9\x01\x02\x01\x00\x03\x03\x9A', 
                          "reply":11, 
                          },
               "proj_timer":{"command":b'\xA9\x01\x13\x01\x00\x00\x13\x9A',
@@ -41,18 +50,26 @@ commands = { "proj_status":{"command":b'\xA9\x01\x02\x01\x00\x03\x03\x9A',
                          },
               "avr_status":{"command":b'\x02\x03\xA0\x82\x00\xDB',
                          "reply":11                        
-                         }                         
+                         }
+                         
               }
-              
-              
+        
+#              
 #A9 01 02 01 00 03 03 9A
+#power on \xA9\x17\x2E\x00\x00\x00\x3F\x9A
+#off \xA9\x17\x2F\x00\x00\x00\x3F\x9A
+
+
+
+#{"POWR OFF", {0xA9, 0x17, 0x2F, 0x00, 0x00, 0x00, 0x3F, 0x9A}},
+
+
 #lamptimer
 #\xA9\x01\x13\x01\x00\x00\x13\x9A
 
 
 ############
 def on_message(client, userdata, message):
-    print("hej")
     bytes_sent = port.write(commands[str(message.payload.decode("utf-8"))]["command"])
     #print commands[str(message.payload.decode("utf-8"))]["command"]
     #print("message received " ,str(message.payload.decode("utf-8")))
@@ -60,10 +77,10 @@ def on_message(client, userdata, message):
     #print("message qos=",message.qos)
     #print("message retain flag=",message.retain)
 ########################################
-broker_address="192.168.1.87"
+broker_address=""
 #broker_address="iot.eclipse.org"
 print("creating new instance")
-client = mqtt.Client("P1") #create new instance
+client = mqtt.Client("projector") #create new instance
 client.on_message=on_message #attach function to callback
 print("connecting to broker")
 client.connect(broker_address) #connect to broker
@@ -71,8 +88,6 @@ client.loop_start() #start the loop
 print("Subscribing to topic","projSerialIn")
 client.subscribe("projSerialIn")
 print("Publishing to topic","projSerialOut")
-
-
 
 #bytes_sent = port.write(commands["avr_status"]["command"])
 
@@ -113,43 +128,3 @@ while port.isOpen():
                 
                 client.publish("projSerialOut",output_hex)
                 client.publish("proj_timer", int.from_bytes(output[4:6], "big"))
-
-
-
-
-
-
-
-
- # result = output
-                    # startCode=bytes(output[0:2])
-                    # print(startCode)
-                    # PdcCmd = bytes(result[2:4])
-                    # print(PdcCmd)
-                    # Data = bytes(result[4:6])
-                    # print(Data)   
-                    # itemAck = bytes(result[1:3])
-                    # #replyType = bytes([result[3]])
-                    # rcvData = bytes(result[4:6])
-                    # #checksum = bytes([result[6]])
-                    # #endCode= bytes([result[7]])
-                    # #print(validResponses[startCode][PdcCmd][Data])
-                    # print(validResponses[b'\x02\x07'][b'\xa8\x82'][b'\x10'])
-
-
-
-   # out = ''
-    #while port.inWaiting() > 0:
-     #    print("reading")
-      #   out += port.read(1)
-    #if out == '':
-     # print("2")
-     # continue
-    # time.sleep(POLL_RATE)
-    # print(out)  
-    # client.publish("avrSerialOut","hej")
-    
-    # time.sleep(POLL_RATE)
-   #   
-   #print "port closed"
-   #ser.close()
